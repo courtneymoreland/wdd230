5d5a53e68e4de7480e2659ba355d80c3e6e4fa2f
courtneymoreland <131329134+courtneymoreland@users.noreply.github.com>
2023-09-28 02:47:27 +0500

    assignment 03

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,16 @@
 document.getElementById("year").innerHTML = new Date().getFullYear()
 document.getElementById("updated").innerHTML = "Last updated: "+document.lastModified
+
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    var icon = document.getElementById("menu-icon");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+        icon.classList.remove("fa-bars");
+        icon.classList.add("fa-times");
+    } else {
+        x.className = "topnav";
+        icon.classList.remove("fa-times");
+        icon.classList.add("fa-bars");
+    }
+}
