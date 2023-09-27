c1565cf7d40b44ea4ef4439b32b4dab326544813
courtneymoreland <131329134+courtneymoreland@users.noreply.github.com>
2023-09-28 02:56:09 +0500

    navbar issue fixed

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,22 @@
 document.getElementById("year").innerHTML = new Date().getFullYear()
 document.getElementById("updated").innerHTML = "Last updated: "+document.lastModified
 
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    var icon = document.getElementById("menu-icon");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-        icon.classList.remove("fa-bars");
-        icon.classList.add("fa-times");
-    } else {
-        x.className = "topnav";
-        icon.classList.remove("fa-times");
-        icon.classList.add("fa-bars");
-    }
-}
+
+document.addEventListener('DOMContentLoaded', function () {
+    var menuToggle = document.getElementById('menu-toggle');
+    var topnav = document.getElementById('myTopnav');
+    var icon = document.getElementById('menu-icon');
+
+    menuToggle.addEventListener('click', function () {
+        if (topnav.className === 'topnav') {
+            topnav.className += ' responsive';
+            icon.classList.remove('fa-bars');
+            icon.classList.add('fa-times');
+        } else {
+            topnav.className = 'topnav';
+            icon.classList.remove('fa-times');
+            icon.classList.add('fa-bars');
+        }
+    });
+});
+
